d1d889b2dce70ee6c4c420b51a089d078ea3efd6
PonnyBonnie1987 <110581902+PonnyBonnie1987@users.noreply.github.com>
2022-08-04 10:31:39 +0700

    Add files via upload

---
+++ Exercise4_Thanachot_P.py
@@ -0,0 +1,10 @@
+FE = 2.50
+GB = 3.00
+ICS = 2.54
+CP = 3.84
+
+print("--- Your Score ---")
+print("Foundation English : ", FE)
+print("General Business : ", GB)
+print("Introduction to Computer Systems : ", ICS)
+print("Computer Programming : ", CP)
