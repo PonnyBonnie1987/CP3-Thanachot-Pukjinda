024049af78ec01ac6cdeb082f0a03462d72c85c6
PonnyBonnie1987 <110581902+PonnyBonnie1987@users.noreply.github.com>
2022-08-09 11:36:40 +0700

    Add files via upload

---
+++ Lecture50.py
@@ -0,0 +1,13 @@
+def AddNumber(x,y):
+    print(x + y)
+def DelNumber(x,y):
+    print(x - y)
+def MultiNumber(x,y):
+    print(x * y)
+def DivNumber(x,y):
+    print(x / y)
+
+AddNumber(10,20)
+DelNumber(10,20)
+MultiNumber(10,20)
+DivNumber(10,20)
