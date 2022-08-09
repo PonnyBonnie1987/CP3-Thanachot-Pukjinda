54d0e5dab9e4ce306fb19bd428ed16fbf05a378f
PonnyBonnie1987 <110581902+PonnyBonnie1987@users.noreply.github.com>
2022-08-09 11:38:48 +0700

    Add files via upload

---
+++ Exercise9_Thanachot_P.py
@@ -0,0 +1,6 @@
+Username = input("UserName : ")
+Password = input("Password : ")
+while Username != "Admin" or Password != "1234":
+    print("Username และ Password ไม่ถูกต้อง")
+    Username = input("UserName : ")
+    Password = input("Password : ")
